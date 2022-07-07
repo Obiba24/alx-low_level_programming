ec8d3fa4ab5fb972d86cc13992bdac8e429151d7
Obiba24 <abdulazizosuman6@gmail.com>
2022-07-07 15:03:04 -0700

    C Debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+* Return: 0
+ */
+
+int main(void)
+{
+        int i;
+
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+
+/*while (i < 10)*/
+/*{*/
+/*putchar(1):*/
+/*{*/
+
+        printf("Infinte loop avoided! \\o/\n");
+
+        return (0);
+}
