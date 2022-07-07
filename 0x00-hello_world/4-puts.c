93e34245b0c56adc57dfad79b485a9f3bb3ba7a5
Obiba24 <abdulazizosuman6@gmail.com>
2022-07-07 09:54:39 -0700

    C Programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * *main - entry point.
- * *Description: Prints the description specified
- * *Return: always zero(success)
- * */
+  *main - entry point.
+  *Description: Prints the description specified
+  *Return: always zero(success)
+  */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
