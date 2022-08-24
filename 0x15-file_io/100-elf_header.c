a84191e1e0cd6fb5b7b1c31664ff85d84f62b5ca
Obiba24 <abdulazizosuman6@gmail.com>
2022-08-24 14:16:14 -0700

    task

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -62,38 +62,38 @@ void print_type(char *ptr)
 
 	if (ptr[5] == 1)
 		type = ptr[16];
-	else
-		type = ptr[17];
-
+OBOBOBOB	else
+OB		type = ptr[17];
+OBOB
 	printf("  Type:                              ");
-	if (type == 0)
-		printf("NONE (No file type)\n");
-	else if (type == 1)
-		printf("REL (Relocatable file)\n");
-	else if (type == 2)
+OBOBOBOB	if (type == 0)
+OBOB		printf("NONE (No file type)\n");
+OBOB	else if (type == 1)
+OBOB		printf("REL (Relocatable file)\n");
+OBOB	else if (type == 2)
 		printf("EXEC (Executable file)\n");
-	else if (type == 3)
-		printf("DYN (Shared object file)\n");
+OBOBOB	else if (type == 3)
+OBOB		printf("DYN (Shared object file)\n");
 	else if (type == 4)
-		printf("CORE (Core file)\n");
-	else
-		printf("<unknown: %x>\n", type);
-}
+OB		printf("CORE (Core file)\n");
+OB	else
+OB		printf("<unknown: %x>\n", type);
+OB}
 
 /**
- * print_osabi - prints osabi
+OB * print_osabi - prints osabi
  * @ptr: magic.
  * Return: no return.
  */
-void print_osabi(char *ptr)
+OBvoid print_osabi(char *ptr)
 {
-	char osabi = ptr[7];
+OB	char osabi = ptr[7];
 
-	printf("  OS/ABI:                            ");
+OB	printf("  OS/ABI:                            ");
 	if (osabi == 0)
-		printf("UNIX - System V\n");
+OB		printf("UNIX - System V\n");
 	else if (osabi == 2)
-		printf("UNIX - NetBSD\n");
+OB		printf("UNIX - NetBSD\n");
 	else if (osabi == 6)
 		printf("UNIX - Solaris\n");
 	else
@@ -105,54 +105,54 @@ void print_osabi(char *ptr)
 
 /**
  * print_version - prints version
- * @ptr: magic.
- * Return: no return.
+OBOBOBOBOBOBOBOB * @ptr: magic.
+OBOB * Return: no return.
  */
-void print_version(char *ptr)
+OBOBOBvoid print_version(char *ptr)
 {
-	int version = ptr[6];
+OBOBOBOBOBOBOBOB	int version = ptr[6];
+OBOBOB
+OBOB	printf("  Version:                           %d", version);
 
-	printf("  Version:                           %d", version);
-
-	if (version == EV_CURRENT)
+OBOBOBOBOB	if (version == EV_CURRENT)
 		printf(" (current)");
 
-	printf("\n");
-}
+OB	printf("\n");
+OBOB}
 /**
- * print_data - prints data
+OB * print_data - prints data
  * @ptr: magic.
- * Return: no return.
+OBOB * Return: no return.
  */
-void print_data(char *ptr)
+OBvoid print_data(char *ptr)
 {
-	char data = ptr[5];
+OB	char data = ptr[5];
 
 	printf("  Data:                              2's complement");
 	if (data == 1)
-		printf(", little endian\n");
+OB		printf(", little endian\n");
 
 	if (data == 2)
 		printf(", big endian\n");
 }
-/**
+OBOBOBOBOB/**
  * print_magic - prints magic info.
- * @ptr: magic.
- * Return: no return.
- */
-void print_magic(char *ptr)
-{
+OBOB * @ptr: magic.
+OBOBOBOBOBOBOB * Return: no return.
+OBOBOB */
+OBOBOBvoid print_magic(char *ptr)
+OBOB{
 	int bytes;
-
+OBOBOBOB
 	printf("  Magic:  ");
 
 	for (bytes = 0; bytes < 16; bytes++)
-		printf(" %02x", ptr[bytes]);
-
+OB		printf(" %02x", ptr[bytes]);
+OBOBOBOB
 	printf("\n");
-
+OB
 }
-
+OBOBOBOB
 /**
  * check_sys - check the version system.
  * @ptr: magic.
@@ -165,82 +165,82 @@ void check_sys(char *ptr)
 	if (sys == '0')
 		exit(98);
 
-	printf("ELF Header:\n");
-	print_magic(ptr);
-
+OBOBOBOB	printf("ELF Header:\n");
+OB	print_magic(ptr);
+OBOB
 	if (sys == '1')
-		printf("  Class:                             ELF32\n");
-
-	if (sys == '2')
-		printf("  Class:                             ELF64\n");
-
+OBOBOBOB		printf("  Class:                             ELF32\n");
+OBOB
+OB	if (sys == '2')
+OBOB		printf("  Class:                             ELF64\n");
+OBOB
 	print_data(ptr);
-	print_version(ptr);
+OBOBOB	print_version(ptr);
 	print_osabi(ptr);
 	print_type(ptr);
 	print_addr(ptr);
-}
+OBOBOB}
 
-/**
+OBOB/**
  * check_elf - check if it is an elf file.
- * @ptr: magic.
+OBOBOB * @ptr: magic.
  * Return: 1 if it is an elf file. 0 if not.
- */
+OB */
 int check_elf(char *ptr)
-{
+OB{
 	int addr = (int)ptr[0];
-	char E = ptr[1];
+OB	char E = ptr[1];
 	char L = ptr[2];
-	char F = ptr[3];
+OB	char F = ptr[3];
 
 	if (addr == 127 && E == 'E' && L == 'L' && F == 'F')
 		return (1);
-
+OB
 	return (0);
 }
 
 /**
  * main - check the code for Holberton School students.
- * @argc: number of arguments.
+OB * @argc: number of arguments.
  * @argv: arguments vector.
  * Return: Always 0.
- */
-int main(int argc, char *argv[])
-{
+OBOBOBOBOBOB */
+OBint main(int argc, char *argv[])
+OBOBOB{
 	int fd, ret_read;
-	char ptr[27];
-
-	if (argc != 2)
+OBOB	char ptr[27];
+OBOB
+OBOB	if (argc != 2)
 	{
-		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
-		exit(98);
-	}
-
+OBOB		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
+OBOB		exit(98);
+OB	}
+OBOB
 	fd = open(argv[1], O_RDONLY);
-
-	if (fd < 0)
+OB
+OBOB	if (fd < 0)
 	{
-		dprintf(STDERR_FILENO, "Err: file can not be open\n");
+OB		dprintf(STDERR_FILENO, "Err: file can not be open\n");
 		exit(98);
 	}
-
+OBOB
 	lseek(fd, 0, SEEK_SET);
-	ret_read = read(fd, ptr, 27);
+OBOBOB	ret_read = read(fd, ptr, 27);
 
-	if (ret_read == -1)
+OBOB	if (ret_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Err: The file can not be read\n");
+OB		dprintf(STDERR_FILENO, "Err: The file can not be read\n");
 		exit(98);
-	}
+OB	}
 
-	if (!check_elf(ptr))
+OB	if (!check_elf(ptr))
 	{
-		dprintf(STDERR_FILENO, "Err: It is not an ELF\n");
+OB		dprintf(STDERR_FILENO, "Err: It is not an ELF\n");
 		exit(98);
-	}
+OB	}
 
 	check_sys(ptr);
 	close(fd);
-
+OB
 	return (0);
 }
